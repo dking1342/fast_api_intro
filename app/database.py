3f755286e5c127615443eebed01cc82960ff2871
David <leanmachine42@yahoo.com>
2022-04-22 01:13:01 -0500

    made env files

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://davidking:auth_string@localhost/fastapitest"
+load_dotenv()
+db_username = os.environ.get("DB_USERNAME")
+db_password = os.environ.get("DB_PASSWORD")
+db_server = os.environ.get("HOST_SERVER")
+db_name = os.environ.get("DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URL = "postgresql://{username}:{password}@{server}/{database}".format(username=db_username, password=db_password, server=db_server, database=db_name)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
